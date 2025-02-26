d3b043b6b4e3b44ad6db87c0b617dfa28b3f2b38
Mukesh <mukeshdixena123@gmail.com>
2025-02-27 04:56:32 +0530

    updated the database config

--- src/util/database.js
+++ src/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('expence-app', 'root', 'Chintu2@2002', {
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     dialect: 'mysql',
     host: 'localhost'
 });
