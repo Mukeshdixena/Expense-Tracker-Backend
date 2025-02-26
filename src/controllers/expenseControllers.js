05ae11ead1854df95fd14599329689efe8c358af
Mukesh <mukeshdixena123@gmail.com>
2025-02-26 13:01:36 +0530

    using aws service

--- src/controllers/expenseControllers.js
+++ src/controllers/expenseControllers.js
@@ -1,11 +1,60 @@
 const expense = require('../models/expense');
 const user = require('../models/user');
 const sequelize = require('../util/database');
+const AWS = require('aws-sdk');
 
 exports.getExpense = async (req, res, next) => {
     try {
         const expenses = await req.user.getExpenses();
         res.status(200).json(expenses);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong!' });
+    }
+};
+
+function uploadToS3(data, fileName) {
+    const BUCKET_NAME = process.env.BUCKET_NAME;
+    const IAM_USER_KEY = process.env.IAM_USER_KEY;
+    const IAM_USER_SECRET_KEY = process.env.IAM_USER_SECRET_KEY;
+
+    const s3 = new AWS.S3({
+        accessKeyId: IAM_USER_KEY,
+        secretAccessKey: IAM_USER_SECRET_KEY,
+    });
+
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: fileName,
+        Body: data,
+        ACL: 'public-read',  // Optional: If you want the file to be publicly accessible
+    };
+
+    return new Promise((resolve, reject) => {
+        s3.upload(params, (err, s3Response) => {
+            if (err) {
+                console.error('Something went wrong:', err);
+                reject(err);
+            } else {
+                console.log("Upload successful:", s3Response.Location);
+                resolve(s3Response.Location);
+            }
+        });
+    });
+}
+
+exports.getExpenseFile = async (req, res, next) => {
+    try {
+        const expenses = await req.user.getExpenses();
+        const stringifiedExpenses = JSON.stringify(expenses);
+        const userId = req.user.id;
+        const fileName = `Expense_${userId}_${new Date().toISOString().replace(/[:.]/g, "-")}.txt`;
+        // const fileName = 'Expense.txt';
+
+        // Wait for S3 upload to complete and get the file URL
+        const fileUrl = await uploadToS3(stringifiedExpenses, fileName);
+
+        res.status(200).json({ fileUrl, success: true });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Something went wrong!' });
--- src/router/expenseRouter.js
+++ src/router/expenseRouter.js
@@ -5,6 +5,7 @@ const userAuth = require('../middleware/auth.js');
 const router = express.Router();
 
 router.get('/api/getExpense', userAuth.authonticate, expenseController.getExpense);
+router.get('/api/getExpenseFile', userAuth.authonticate, expenseController.getExpenseFile);
 router.post('/api/postExpense', userAuth.authonticate, expenseController.postExpense);
 router.delete('/api/deleteExpense/:expenseId', userAuth.authonticate, expenseController.deleteExpense);
 router.patch('/api/editExpense/:editId', userAuth.authonticate, expenseController.editExpense);
