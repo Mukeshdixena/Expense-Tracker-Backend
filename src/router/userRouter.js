1adf29659cfc065308558947d1f9875e0cd5a221
Mukesh <mukeshdixena123@gmail.com>
2025-04-10 11:05:03 +0530

    [WIP]

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/api/getUser', userController.getUser);
 router.get('/api/getLeaderBoad', userController.getLeaderBoad);
 router.get('/api/getUserById', userAuth.authenticate, userController.getUserById);
-router.get('/api/getUserTotalAmount', userAuth.authenticate, userController.getUserTotalAmount);
+router.get('/api/getUserTotalAmount', userAuth.authenticate, userController.getUserTotalAmount);//
 router.post('/api/postUser', userController.postUser); //
 router.patch('/api/postUserPass', userController.postUserPass);//
 router.delete('/api/deleteUser/:UserId', userController.deleteUser);
